6277e86408b872a0ad6c44b61b22cb3015fa64e0
ziyanmomin <ziyanmomin93@gmail.com>
2026-04-29 21:08:55 +0530

    added CI/CD pipeline

--- index.js
+++ index.js
@@ -18,6 +18,3 @@ app.get("/secret", (req, res) => {
 app.listen(PORT, () => { 
   console.log("Server running on port " + PORT); 
 });
-app.get("/test", (req, res) => {
-  res.send("CI/CD is working for auto-development");
-});
